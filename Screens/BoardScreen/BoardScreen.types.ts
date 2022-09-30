965ac2e45de36dd7ce62879d9b6c851c5def0825
emeliepetersson <54805510+emeliepetersson@users.noreply.github.com>
2022-09-30 16:47:16 +0200

    add new card images

--- Screens/BoardScreen/BoardScreen.types.ts
+++ Screens/BoardScreen/BoardScreen.types.ts
@@ -3,12 +3,12 @@ export const CARDS = [
 		id: 'card1', image: require('../../assets/card.png')
 	},
 	{ 
-		id: 'card2', image: require('../../assets/card.png')
+		id: 'card2', image: require('../../assets/card2.png')
 	},
 	{ 
-		id: 'card3', image: require('../../assets/card.png')
+		id: 'card3', image: require('../../assets/card3.png')
 	},
 	{ 
-		id: 'card4', image: require('../../assets/card.png')
+		id: 'card4', image: require('../../assets/card4.png')
 	}
 ];
